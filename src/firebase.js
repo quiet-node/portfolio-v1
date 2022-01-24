4f79969d7fdff53522e06631e36fd59dc9e58a8d
lgad31vn <lgad31vn@gmail.com>
2022-01-24 03:34:02 -0500

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBh-XnTaP3y75ufJETKZ7ABA4SHVWH9aDs",
-  authDomain: "react-portfolio-contact-e63ac.firebaseapp.com",
-  projectId: "react-portfolio-contact-e63ac",
-  storageBucket: "react-portfolio-contact-e63ac.appspot.com",
-  messagingSenderId: "488127716812",
-  appId: "1:488127716812:web:fbf2153aae8e218479d169",
+  apiKey: 'AIzaSyBh-XnTaP3y75ufJETKZ7ABA4SHVWH9aDs',
+  authDomain: 'react-portfolio-contact-e63ac.firebaseapp.com',
+  projectId: 'react-portfolio-contact-e63ac',
+  storageBucket: 'react-portfolio-contact-e63ac.appspot.com',
+  messagingSenderId: '488127716812',
+  appId: '1:488127716812:web:fbf2153aae8e218479d169',
 });
 
 var db = firebaseApp.firestore();
